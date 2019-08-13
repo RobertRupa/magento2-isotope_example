101a664a7aa184774508774f596d1627822da44a
Robert Rupa <r.rupa@sintraconsulting.eu>
2019-08-13 21:10:18 +0200

    initial

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright © Konatsu.pl Robert Rupa.
+ */
+
+var config = {
+    paths: {
+        'isotope': 'RobertRupa_IsotopeExample/js/isotope.pkgd.min',
+        'jquery-bridget': 'RobertRupa_IsotopeExample/js/jquery-bridget',
+        'isotopeInit': 'RobertRupa_IsotopeExample/js/isotope-init'
+    }
+};
